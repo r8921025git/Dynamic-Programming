9203e7b8872ca787b1e7e8eafc815aabe30cc5e8
r8921025git <r8921025+git@yahoo.com.tw>
2014-12-18 07:24:04 -0500

    Create longest nondecreasing subsequence.cpp

---
+++ longest nondecreasing subsequence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using std::vector;
+
+vector<int> longest(const vector<int>& A) {
+    vector<int> R;
+    return R;
+}
+
+int main()
+{
+   
+   vector<int> A = {5,3,2,7,1,4};
+   vector<int> R = longest(A);
+   
+   return 0;
+}
+
