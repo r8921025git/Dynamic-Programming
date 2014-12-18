1f194355d969d232b88001221ff275a51dfef02c
r8921025git <r8921025+git@yahoo.com.tw>
2014-12-18 07:37:38 -0500

    Update longest nondecreasing subsequence.cpp

--- longest nondecreasing subsequence.cpp
+++ longest nondecreasing subsequence.cpp
@@ -5,13 +5,32 @@ using std::vector;
 
 vector<int> longest(const vector<int>& A) {
     vector<int> R;
+    const int N = A.size();
+    R.resize(N);
+    for (int i=0; i<N; ++i) {
+        R[i]=1;
+        for (int j=0; j<i; ++j) {
+            if (A[j]<=A[i]) {
+                if (R[j]+1>=R[i]) {
+                    R[i] = R[j]+1;
+                }
+            }
+        }
+    }
+    int maxLen = 0;
+    for (int i=0; i<N; ++i) {
+        if (R[i]>maxLen) {
+            maxLen = R[i];
+        }
+    }
+    printf("maxLen=%d\n",maxLen);
     return R;
 }
 
 int main()
 {
    
-   vector<int> A = {5,3,2,7,1,4};
+   vector<int> A = {5,3,2,7,1,4,8};
    vector<int> R = longest(A);
    
    return 0;
